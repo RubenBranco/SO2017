6d359ba16f3c35366c38d403b7a31f4171a02db4
rubenbranco <ruben.branco@outlook.pt>
2017-10-24 20:02:13 +0100

    Process - Zip and Unzip
    - Fiz zip e unzip
    - Falta -t option, integrar uma pipe ou queue para cada inicio do ciclo ir ver se algum processo teve erro

--- pzip.py
+++ pzip.py
@@ -3,18 +3,49 @@
 import argparse
 from zipfile import ZipFile
 from multiprocessing import Process, Value, Array
-import ctypes
-
+from ctypes import c_char_p
+from multiprocessing import Semaphore
 
 class PZip:
-    def __init__(self, files, mode, t, processes=1):
-        self.files = Array(ctypes.c_char_p, files)
+    def __init__(self, files, mode, t, processes):
+        self.files = Array(c_char_p, len(files))
+        self.pointer = Value("i", 0)
+        self.file_init(files)
         self.processes = processes
         self.mode = mode
         self.t = t
-        file1 = self.files.pop()
-        print(file1)
-        print(self.files[0])
+        for i in range(processes):
+            newP = Process(target=(self.zip if self.mode == 'c' else self.unzip))
+            newP.start()
+
+    def file_init(self, files):
+        for i in range(len(files)):
+            self.files[i] = files[i]
+
+    def zip(self):
+        while self.pointer.value < len(self.files):
+            sem = Semaphore(1)
+            sem.acquire()
+            iterator = self.pointer.value
+            self.pointer.value += 1
+            sem.release()
+            if iterator < len(self.files):
+                File = self.files[iterator]
+                with ZipFile(File + '.zip', 'w') as zipfile:
+                    zipfile.write(File)
+
+    def unzip(self):
+        while self.pointer.value < len(self.files):
+            sem = Semaphore(1)
+            sem.acquire()
+            iterator = self.pointer.value
+            self.pointer.value += 1
+            sem.release()
+            if iterator < len(self.files):
+                File = self.files[iterator]
+                with ZipFile(File, 'r') as zipfile:
+                    zipfile.extractall('.')
+
 if __name__ == '__main__':
     description = 'Comprime e descomprime conjuntos de ficheiros paralelamente'
     parser = argparse.ArgumentParser(description=description)
@@ -22,7 +53,7 @@ if __name__ == '__main__':
     group.add_argument("-c", dest="mode", help="Comprimir ficheiros", action="store_const", const="c")
     group.add_argument("-d", dest="mode", help="Descomprimir ficheiros", action="store_const", const="d")
     parser.add_argument("-p", metavar="configs", dest="parallel", help="Numero de processos permitidos", type=int,
-                        nargs=1)
+                        nargs=1, default=1)
     parser.add_argument("-t", dest="t", help="Obriga a suspensao de execucao caso um ficheiro seja"
                                                                 "nao existente", action="store_true")
     parser.add_argument("files", type=str, metavar="files", nargs="+", help="Ficheiros para comprimir/descomprimir")
